04d03c92bc3a648b13781074e59a6739bf8884df
Edo Hussl <edo@hussl.nl>
2023-03-07 16:43:55 +0100

    Fix ids

--- src/app/team-results/team-results.component.spec.ts
+++ src/app/team-results/team-results.component.spec.ts
@@ -1,6 +1,5 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { TeamResultsComponent } from './team-results.component';
